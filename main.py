40208a13e0f127a2b8e56261e1496548d52ff100
Malith Madushan Liyanage <99232266+IT21280474@users.noreply.github.com>
2024-11-30 19:42:25 +0530

    second commit

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ app = FastAPI()
 def index():
     return {'data':'Blog List'}
 
-@app.get('/blog/id')
-def show():
+@app.get('/blog/{id}')
+def show(id: int):
+    #fatch blog with id = id
     return {'data': id}
+
+
+@app.get('/blog/unpublished')
+def comments():
+    
+    return{'data':'all unpublished  blogs'}
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fatch comments of blog with id = id
+    return{'data':{'1','2'}}
